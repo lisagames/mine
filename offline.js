35f4f52b24312aaef5a399c48353f8ea822745a6
lisagames <73744447+lisagames@users.noreply.github.com>
2020-11-02 22:21:13 +0200

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1604348207,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/fon.png",
+		"images/blocks.png",
+		"images/player-sheet0.png",
+		"images/lighting-sheet0.png",
+		"images/hud_block-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"penumbra.png"
+	]
+}
